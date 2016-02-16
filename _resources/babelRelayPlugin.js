0fdabc154ac17cd8de440b2b61ec8442051d5f91
Adam Miskiewicz <adam@sk3vy.com>
2016-02-16 15:26:51 -0500

    adding a 'fetch-graphql' script

--- _resources/babelRelayPlugin.js
+++ _resources/babelRelayPlugin.js
@@ -1,4 +1,4 @@
 const getBabelRelayPlugin = require('babel-relay-plugin');
-const schema = require('/devel/exponent/oss/react-conf-2016-server/lib/schema.json');
+const schema = require('./schema.json');
 
 module.exports = getBabelRelayPlugin(schema.data);
